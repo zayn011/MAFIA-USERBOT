0691b3df650e153fa4f71911f3f7c12ec24b9aee
zayn011 <85694862+zayn011@users.noreply.github.com>
2021-06-10 21:05:12 +0530

    Update adultzone.py

--- userbot/plugins/adultzone.py
+++ userbot/plugins/adultzone.py
@@ -31,26 +31,20 @@ async def boobs(event):
     await a.delete()
 
 
-@bot.on(admin_cmd("butts$"))
-@bot.on(sudo_cmd(pattern="butts$", allow_sudo=True))
+@bot.on(admin_cmd("skull$"))
+@bot.on(sudo_cmd(pattern="skull$", allow_sudo=True))
 async def butts(event):
     if event.fwd_from:
         return
     if not os.path.isdir(Var.TEMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Var.TEMP_DOWNLOAD_DIRECTORY)
     pic_loc = os.path.join(Var.TEMP_DOWNLOAD_DIRECTORY, "butts.jpg")
-    a = await event.reply("Finding some beautiful butts for u🧐")
-    await asyncio.sleep(0.5)
-    await a.edit("Sending some beautiful butts🤪")
-    nsfw = requests.get("http://api.obutts.ru/noise/1").json()[0]["preview"]
-    urllib.request.urlretrieve("http://media.obutts.ru/{}".format(nsfw), pic_loc)
-    await event.client.send_file(event.chat_id, pic_loc, force_document=False)
-    os.remove(pic_loc)
-    await event.delete()
-    await a.delete()
-
+    a = await event.reply("Tum skull hum insaan hum jhopdiwale skull Tum bhosiwale")
+    await asyncio.sleep(2.0)
+    await a.edit("Kbhi kbhi meri dil me khyaal ata hai ayse chutiya skull jese logon ko kon paida kr jata hai😂.")
+   
 CmdHelp("adultzone").add_command(
   'boobs', None, 'Sends a random boobs pic'
 ).add_command(
-  'butts', None, 'Sends a random Butt pic'
+  'skull', None, 'Sends a random Butt pic'
 ).add()
